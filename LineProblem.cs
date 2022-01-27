53d47349483843a427355d52cfd30bcd38ef7165
1000007201 <nishant.sharma5504@gmail.com>
2022-01-28 00:35:50 +0530

    [Nishant] Add:Line are equal done

--- LineProblem.cs
+++ LineProblem.cs
@@ -8,10 +8,32 @@ namespace LineComparisonProblem
 {
     public class LineProblem
     {
-        public void GetLineLength(int x1,int y1,int x2,int y2)
+        private double line1Length, line2Length;
+        public void GetLine1Length(int x1,int y1,int x2,int y2)
         {
-            double lineLength = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y2),2));
-            Console.WriteLine("Length of a line is {0}",lineLength);
+            line1Length = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y2),2));
+        }
+
+        public void GetLine2Length(int x1, int y1, int x2, int y2)
+        {
+            line2Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y2), 2));
+        }
+        public void CompareLine()
+        {
+            if (line1Length > line2Length)
+            {
+                Console.WriteLine("Line 1 is longer than line 2");
+
+            }
+            if (line1Length < line2Length)
+            {
+                Console.WriteLine("Line 2 is longer than Line 1");
+            }
+            if (line1Length == line2Length)
+            {
+                Console.WriteLine("Both line are equal in length");
+            }
         }
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LineComparisonProblem;
 LineProblem line = new LineProblem();
-line.GetLineLength(2,2,4,4);
+line.GetLine1Length(2,2,4,4);
+line.GetLine2Length(3, 2, 8, 4);
+line.CompareLine();
